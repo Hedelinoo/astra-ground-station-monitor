e4e85eb906cc32a3932dbf9b2c37f8aa25c4f539
Hubert <70658889+HubertJan@users.noreply.github.com>
2024-05-19 21:09:49 +0200

    Improve file-to-db.py

--- file-to-db.py
+++ file-to-db.py
@@ -7,21 +7,60 @@ def initialize_db(db_path):
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
 
-    cursor.execute('''CREATE TABLE IF NOT EXISTS raw_serial_data (
-                        id INTEGER PRIMARY KEY,
-                        packet STRING NOT NULL,
-                        sentRecieved STRING NOT NULL);''')
+    # Create tables if they do not exist
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS packets (
+        id INTEGER PRIMARY KEY,
+        packet_id INTEGER NOT NULL,
+        temperature1 INTEGER,
+        temperature2 INTEGER,
+        temperature3 INTEGER,
+        temperature4 INTEGER,
+        temperature5 INTEGER,
+        temperature6 INTEGER,
+        temperature7 INTEGER,
+        temperature8 INTEGER,
+        temperature9 INTEGER,
+        temperature10 INTEGER,
+        temperature11 INTEGER,
+        temperature12 INTEGER,
+        temperature13 INTEGER,
+        temperature14 INTEGER,
+        sentReceived STRING NOT NULL
+    );''')
     
     return conn, cursor
+
+def handle_data_packet(packet: str, cursor):
+    data = packet.split(';')[1:]
+    packet_id = int(data[0])
+    packet_temperatures = []
+    
+    for d in data[1:]:
+        if not d.isnumeric():
+            break
+        packet_temperatures.append(int(d) - 127)
+
+    temperature_values = [None] * 14
+    if packet_id == 4:
+        temperature_values[:7] = packet_temperatures[:7]
+    elif packet_id == 5:
+        temperature_values[7:] = packet_temperatures[:7]
+    elif packet_id == 6:
+        temperature_values[14-1] = packet_temperatures[0]
+    
+    cursor.execute('''
+    INSERT INTO packets (packet_id, temperature1, temperature2, temperature3, temperature4, temperature5, temperature6, temperature7, temperature8, temperature9, temperature10, temperature11, temperature12, temperature13, temperature14, sentReceived)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+    ''', (packet_id, *temperature_values, 'received'))
+    cursor.connection.commit()
 
 def read_file_and_write_to_db(file_path, cursor):
     with open(file_path, 'r') as file:
         for line in file:
             line_data = line.strip()
             if line_data:  # Ensure the line is not empty
-                cursor.execute("INSERT INTO raw_serial_data (packet, sentRecieved) VALUES (?, ?);",
-                               (line_data, 'received'))
-                cursor.connection.commit()
+                handle_data_packet(line_data, cursor)
 
 def main():
     parser = argparse.ArgumentParser(description="Read a file and write its contents into a database.")
